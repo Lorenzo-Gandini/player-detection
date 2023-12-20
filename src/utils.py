62508f7c61bf081e6c64a9f7d54d30c80ab837f4
Lorenzo Gandini <lorenzo.gandini@hotmail.it>
2023-12-20 17:41:28 +0100

    Add varibales to utils

--- src/utils.py
+++ src/utils.py
@@ -3,18 +3,25 @@ import cv2
 import imutils
 import argparse
 from imutils.object_detection import non_max_suppression
+from scipy import stats
+import time
 
-# Parametri dei kernel per il pre-processing dell'immagine in court detection
-lower_green = np.array([30, 30, 30])            # Valori minimi di H, S, V per il verde
-upper_green = np.array([70, 255, 255])          # Valori massimi di H, S, V per il verde
-blur_kernel = (15, 15)                          #Kernel da applicare per il denoising con blur
-erosion_kernel = np.ones((7, 7), np.uint8)      #kernel da applicare per l'erosione
+original = cv2.imread('data/img/frame.png')
+image = original.copy()
+
+## --- START VARIABLES BLOCK ---
+# Green bounds for court removing
+lower_green = np.array([30, 30, 30]) 
+upper_green = np.array([70, 255, 255]) 
+
+# Parameters of Daimler HOG
+winSize = (48, 96)  
+blockSize = (16, 16)  
+blockStride = (8, 8)  
+cellSize = (8, 8)  
+nbins = 9  
+
+# Kernels for court operations
+erosion_kernel = np.ones((7, 7), np.uint8) #kernel da applicare per l'erosione
 dilatation_kernel = np.ones((13, 13), np.uint8) #Kernel da applciare per la dilatazione
-
-
-#--- PARAMETRI INIZIALI PER HOG CON DAIMLER DATASET ---
-winSize = (48, 96)  # Esempio, da adattare in base alle specifiche di Daimler
-blockSize = (16, 16)  # Esempio
-blockStride = (8, 8)  # Esempio
-cellSize = (8, 8)  # Esempio
-nbins = 9  # Esempio
+blur_kernel = (15, 15) #Kernel da applicare per il denoising
