576303a91935417ffac8f0fdd512caaf2082ae96
Lorenzo Gandini <lorenzo.gandini@hotmail.it>
2024-01-02 11:22:30 +0100

    New function for frame detection

--- src/courtDetectorImg.py
+++ src/courtDetectorImg.py
@@ -1,40 +1,50 @@
 
 from utils import *
 
+def process_frame(analyzed_image):
+    '''
+    This function takes as input an image ad returns the same image with the draw of the court detected
+    '''
+    # Converte l'immagine in formato HSV così da poter analizzare il canale h
+    hsv = cv2.cvtColor(analyzed_image, cv2.COLOR_BGR2HSV)
+
+    # Crea una maschera binaria per il colore verde, così da andare a indetificare solo il campo da calcio
+    green_mask = cv2.inRange(hsv, lower_green, upper_green)
+
+    # Effettua un denoising alla maschera, con un filtro, l'erosione dei residui e la dilatazione dell'area bianca
+    # Così da ridurre le linee del campo e minimizzare gli ostacoli ai bordi dell'area. 
+    blurred_image = cv2.GaussianBlur(green_mask, blur_kernel, 0) 
+    erosion = cv2.erode(blurred_image, erosion_kernel, iterations=1)
+    dilatation = cv2.dilate(erosion, dilatation_kernel, iterations=1)
+
+    # Trova i contorni nell'immagine binaria filtrata
+    contours, _ = cv2.findContours(dilatation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Disegna i contorni sull'immagine originale
+    # Definition of the Area
+    max_area = 0
+    max_contour = None
+
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if area > max_area:
+            max_area = area
+            max_contour = cnt
+
+    if max_contour is not None:
+        epsilon = 0.005 * cv2.arcLength(max_contour, True)
+        approx = cv2.approxPolyDP(max_contour, epsilon, True)
+        cv2.drawContours(analyzed_image, [approx], -1, (0, 255, 0), 3)        
+
+    return analyzed_image
+
+
 # Leggi l'immagine
 image = cv2.imread('data/img/frame.png')
 
-# Converte l'immagine in formato HSV così da poter analizzare il canale h
-hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-
-# Crea una maschera binaria per il colore verde, così da andare a indetificare solo il campo da calcio
-green_mask = cv2.inRange(hsv, lower_green, upper_green)
-
-# Effettua un denoising alla maschera, con un filtro, l'erosione dei residui e la dilatazione dell'area bianca
-# Così da ridurre le linee del campo e minimizzare gli ostacoli ai bordi dell'area. 
-blurred_image = cv2.GaussianBlur(green_mask, blur_kernel, 0) 
-erosion = cv2.erode(blurred_image, erosion_kernel, iterations=1)
-dilatation = cv2.dilate(erosion, dilatation_kernel, iterations=1)
-
-# Trova i contorni nell'immagine binaria filtrata
-contours, _ = cv2.findContours(dilatation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-''' 
-#Threshold per definire l'area. Siccome un campo da calcio occupa l'area maggiore dell'imamgine,
-la soglia sarà un'terzo dell'area totale.
-Previene la selezione di eventuali banner, o cartelloni pubblicitari verdi.
-'''
-threshold_area = 1/3*(image.shape[0]*image.shape[1])
-
-# Disegna i contorni sull'immagine originale
-for cnt in contours:
-    # Filtra contorni troppo piccoli
-    if cv2.contourArea(cnt) > threshold_area:
-        epsilon = 0.001 * cv2.arcLength(cnt, True)
-        approx = cv2.approxPolyDP(cnt, epsilon, True)
-        cv2.drawContours(image, [approx], -1, (0, 255, 0), 2)
+my_image = process_frame(image)
 
 # Visualizza l'immagine risultante
-cv2.imwrite('results/img/court-in-frame.png', image)
+cv2.imwrite('results/img/court-in-frame.png', my_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
