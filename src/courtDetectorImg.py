ef742461c23dd9701729f803e729e5dcf0d4d29d
Lorenzo Gandini <lorenzo.gandini@hotmail.it>
2024-01-09 18:35:13 +0100

    Fixed court detection indentation and comments

--- src/courtDetectorImg.py
+++ src/courtDetectorImg.py
@@ -1,27 +1,28 @@
 
+print("\n + RUNNING : Detection of the court in a single frame.")
 from utils import *
 
 def process_frame(analyzed_image):
     '''
-    This function takes as input an image ad returns the same image with the draw of the court detected
+    This function takes as input an image ad returns the same image with the draw of the court detected, with a green line.
     '''
-    # Converte l'immagine in formato HSV così da poter analizzare il canale h
+
     hsv = cv2.cvtColor(analyzed_image, cv2.COLOR_BGR2HSV)
 
-    # Crea una maschera binaria per il colore verde, così da andare a indetificare solo il campo da calcio
+    # Creation of a green mask, in order to identify the court.
     green_mask = cv2.inRange(hsv, lower_green, upper_green)
 
-    # Effettua un denoising alla maschera, con un filtro, l'erosione dei residui e la dilatazione dell'area bianca
-    # Così da ridurre le linee del campo e minimizzare gli ostacoli ai bordi dell'area. 
+    ''' Denoise the mask with a filter, apply an erosion of the residues and dilates the remaining area. 
+    In this way, the lines will be reduced and minimize the obstacles during the area detection.
+    --- Are used big kernels, since is necessary to enphatize very much '''
     blurred_image = cv2.GaussianBlur(green_mask, blur_kernel, 0) 
     erosion = cv2.erode(blurred_image, erosion_kernel, iterations=1)
     dilatation = cv2.dilate(erosion, dilatation_kernel, iterations=1)
 
-    # Trova i contorni nell'immagine binaria filtrata
+    # Find all the countours in the filtered image
     contours, _ = cv2.findContours(dilatation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    # Disegna i contorni sull'immagine originale
-    # Definition of the Area
+    # Draw the area
     max_area = 0
     max_contour = None
 
@@ -39,12 +40,13 @@ def process_frame(analyzed_image):
     return analyzed_image
 
 
-# Leggi l'immagine
+#Read the image
 image = cv2.imread('data/img/frame.png')
-
 my_image = process_frame(image)
 
-# Visualizza l'immagine risultante
+#Write the image with the court detected
 cv2.imwrite('results/img/court-in-frame.png', my_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+print(" + COMPLETE : Detection of the court in a single frame.\n")
+print(" - You can find the result image in results/img/court-in-frame.png \n")
